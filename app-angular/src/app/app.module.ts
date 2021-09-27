d6fee5d81d900b2019d31fc20389cd0f1e047c1c
israel palma <ipalm001@gmail.com>
2021-09-27 15:00:06 -0300

    Forms Module

---
+++ app-angular/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './compartida/header/header.component';
+import { ProductoComponent } from './general/producto/producto.component';
+import { CategoriaComponent } from './pages/categoria/categoria.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './compartida/not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProductoComponent,
+    CategoriaComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
